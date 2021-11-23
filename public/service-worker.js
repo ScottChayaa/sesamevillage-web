f8cc418d421d41b480a5ba529b09f24de8c0bd5b
ScottLin <ScottLin@gangutech.com>
2021-11-24 00:52:15 +0800

    update

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d1cb771134360aa2b75cb4538174280f.js"
+  "/precache-manifest.0c96b21f032c38e5a6be68764d4d227e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "sesamevillage"});
